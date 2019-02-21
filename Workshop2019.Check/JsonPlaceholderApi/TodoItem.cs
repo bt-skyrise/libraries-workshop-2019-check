c8d805546206d61dae16b35462d9e0035a0eb510
Ryszard Tarajkowski <ryszard.tarajkowski@btsignaal.com>
2019-02-21 16:42:29 +0100

    Implemented check for a workshop.

---
+++ Workshop2019.Check/JsonPlaceholderApi/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Workshop2019.Check.JsonPlaceholderApi
+{
+    public class TodoItem
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
--- Workshop2019.Check/Program.cs
+++ Workshop2019.Check/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Workshop2019.Check.JsonPlaceholderApi;
 
 namespace Workshop2019.Check
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            var api = new JsonPlaceholderApiHttpClient(
+                apiUrl: new Uri(configuration["JsonPlaceholderApiUrl"]),
+                apiKey: configuration["JsonPlaceholderApiKey"]
+            );
+
+            var todoItems = await api.Get<List<TodoItem>>("todos", new Dictionary<string, string>
+            {
+                { "userId", "1" },
+                { "completed", "true" }
+            });
+
+            Console.WriteLine("User 1 todos:");
+            Console.WriteLine();
+
+            foreach (var todoItem in todoItems)
+            {
+                Console.WriteLine($"Id:        {todoItem.Id}");
+                Console.WriteLine($"Title:     {todoItem.Title}");
+                Console.WriteLine($"Completed: {todoItem.Completed}");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
